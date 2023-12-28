cc24ba0f95912cd7cc771986d1f8d159ba46a605
Cristian Vaccarella <vaccarellacristian2@gmail.com>
2023-12-28 15:44:11 +0100

    first test

--- test/stupidCalc_test.py
+++ test/stupidCalc_test.py
@@ -2,11 +2,13 @@ from src.stupidCalc import add
 import requests
 
 url = 'https://api.github.com/repos/apache/commons-io/pulls'
+TOKEN = "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"
+header = {"Authorization": "Bearer " + TOKEN}
 
 def test_add():
     i = add(4,5)
     assert i == 9
 
 def test_api():
-    response = requests.get(url)
+    response = requests.get(url, headers=header)
     assert response.status_code == 200
